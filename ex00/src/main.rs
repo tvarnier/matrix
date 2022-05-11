937ac4f3fab93277ad3c96f07e43de27f530b10f
tvarnier <t.varnier.pro@gmail.com>
2022-05-11 10:42:21 +0200

    Ex00

--- ex00/src/main.rs
+++ ex00/src/main.rs
@@ -1,14 +1,41 @@
-mod structs;
-
-use crate::structs::*;
+mod mat;
+use mat::{matrix::Matrix, vector::Vector};
 
 fn main() {
-    let mut v: Vector<f32> = Vector::new(vec![1., 2., 3.]);
-    v.print();
-    v.add(&Vector::<f32>::new(vec![2., 2., 3.]));
-    v.print();
-    v.sub(&Vector::<f32>::new(vec![2., 2., 3.]));
-    v.print();
-    v.scl(2.);
-    v.print();
+    let mut u = Vector::from([2., 3.]);
+    let v = Vector::from([5., 7.]);
+    u.add(&v);
+    println!("{}", u);
+    // [7.0]
+    // [10.0]
+    let mut u = Vector::from([2., 3.]);
+    let v = Vector::from([5., 7.]);
+    u.sub(&v);
+    println!("{}", u);
+    // [-3.0]
+    // [-4.0]
+    let mut u = Vector::from([2., 3.]);
+    u.scl(2.);
+    println!("{}", u);
+    // [4.0]
+    // [6.0]
+
+    let mut u = Matrix::from([[1., 2.], [3., 4.]]);
+    let v = Matrix::from([[7., 4.], [-2., 2.]]);
+    u.add(&v);
+    println!("{}", u);
+    // [8.0, 6.0]
+    // [1.0, 6.0]
+
+    let mut u = Matrix::from([[1., 2.], [3., 4.]]);
+    let v = Matrix::from([[7., 4.], [-2., 2.]]);
+    u.sub(&v);
+    println!("{}", u);
+    // [-6.0, -2.0]
+    // [5.0, 2.0]
+    let mut u = Matrix::from([[1., 2.], [3., 4.]]);
+    u.scl(2.);
+    println!("{}", u);
+    // [2.0, 4.0]
+    // [6.0, 8.0]
 }
